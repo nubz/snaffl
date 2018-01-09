c1d086ce8a55fb0cb3b301b5c664a5c62a5c941e
Ali <ali@nubz.com>
2018-01-09 15:48:57 +0000

    added more apis

---
+++ imports/api/cardTypes.js
@@ -0,0 +1,3 @@
+import { Mongo } from 'meteor/mongo';
+
+export const CardTypes = new Mongo.Collection('cardTypes');
--- imports/api/cards.js
+++ imports/api/cards.js
@@ -1,3 +1,3 @@
 import { Mongo } from 'meteor/mongo';
- 
+
 export const Cards = new Mongo.Collection('cards');
